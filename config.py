2e371a1880054df306956dbcbe70b370b5d89018
Douglas Queiroz <72622033+douglas-perquisador-dev@users.noreply.github.com>
2024-11-07 19:15:05 -0400

    feat: implement authentication to protect the routes and config seed migration user

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+import secrets
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 # initiating Flask
 app = Flask(__name__)
@@ -10,9 +12,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'postgresql://postgres:D4q15g7b2@localhost:5432/bank_bd')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Config JWT
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', secrets.token_hex(32))  # Defina uma chave secreta segura
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600  # Expiração do token (em segundos)
+
 # initiating BD and the Flask-Migrate
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# import models for migrations
-from models import accounts, transactions
+from models.seeds import seed_user
+
+jwt = JWTManager(app)
