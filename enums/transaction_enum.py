6f4d5c0cfaf840b6754f12675ca28cccb3c1a33d
douglas-perquisador-dev <douglas.doug5040qgb@gmail.com>
2025-02-14 10:13:47 -0400

    docs: adjust comment about project structure

--- enums/transaction_enum.py
+++ enums/transaction_enum.py
@@ -1,3 +1,9 @@
+"""
+ENUMs garantem que o código seja mais claro e menos sujeito a erros, além de facilitar validações. 
+Usaremos o módulo enum do Python para criar o PaymentMethod e ajustaremos o código para utilizar-
+este Enum nos repositórios, serviços e nas views. O Enum FormaPagamento substitui strings cruas, tornando o código mais seguro e legível.
+"""
+
 from enum import Enum
 
 class PaymentMethod(Enum):
